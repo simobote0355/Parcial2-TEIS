cb6e7f9ffed81edae6d02ae4f31bbaec1935448c
unknown <simobotero7445@gmail.com>
2025-04-30 07:05:51 -0500

    App Flask

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def factoriall(n):
+    if n < 0:
+        return "No esta definido para numeros negativos"
+    elif n == 0: 
+        return 1
+    else:
+        result = 1
+        for i in range(1, n+1):
+            result *= i
+        return result
+
+@app.route('/factorial/<number>')
+def factorial_page(number):
+    number = int(number)
+    factorial = factoriall(number)
+    return jsonify({'number': number, 'factorial': factorial})
+    #else:
+    #    return jsonify(error="Numero negativo")
+
+if __name__ == '__main__':
+    app.run(debug=True)
